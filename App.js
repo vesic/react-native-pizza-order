0020c45220fe44aef476a4b514b4d07bf4a3ed3c
dusan.vesic <dusan.vesic@gmail.com>
2017-12-15 07:20:36 +0100

    init commit

---
+++ App.js
@@ -0,0 +1,36 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  ListView,
+  TouchableHighlight,
+  Alert
+} from 'react-native';
+
+
+export default class App extends Component<{}> {
+  render() {
+    return (
+      <Text>Text</Text>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  }
+});
